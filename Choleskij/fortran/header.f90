b0a707feb2ce52696bf79a6fd9603af5aeb01a56
InnaSimakova <74507127+InnaSimakova@users.noreply.github.com>
2020-12-26 10:36:29 +0300

    Delete header.f90

--- Choleskij/fortran/header.f90
+++
@@ -1,9 +0,0 @@
-        module header
-                interface
-                subroutine Choleskij_Decomposition(A, n, comp_mode, b, L, time, error)
-                        integer(4) :: n, comp_mode, b
-                        real(8), dimension(n,n) :: A, L
-                        real(8) :: time, error
-                end subroutine
-                end interface
-        end module
